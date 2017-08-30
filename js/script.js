d149298f710b14463817499897671c1a87e7d2e6
julian.noyon@gmail.com <julian.noyon@gmail.com>
2017-08-30 08:36:17 +0200

    Update from home

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-script.js
+$(document).ready(function(){
+	/** Corporate **/
+	$('.tabpanels__nav__btn').on('click', function(){
+		$('.tabpanels__nav__btn a').removeClass('tabpanels__nav__btn--selected');
+		$(this).find('a').toggleClass('tabpanels__nav__btn--selected');
+	});
+
+
+});
